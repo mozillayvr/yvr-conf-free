c6dc0bc1b0aceb22f2a519aa62e9f48ba7899ee9
Bryan Clark <clarkbw@gmail.com>
2015-02-04 13:28:34 -0800

    cleanups
    remove the views directory, app/index.html is the new hotness
    cleanup the server to just serve a static page
    add a port to the default config and remove the title

--- server.js
+++ server.js
@@ -7,30 +7,12 @@
 
 "use strict";
 
-var path = require('path');
-
 var config = require('config');
 
 var app = require('gcal-conf-free-api');
 
 app.use(require('express').static(__dirname + '/public'));
 
-// HTML WIDGET
-
-app.engine('.html', require('ejs').__express);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: config.get('ics.title')
-  });
-});
-
-app.get('/js/moment.js', function (req,res) {
-  res.sendfile(path.join(__dirname,'node_modules','moment','moment.js'));
-});
-
-var server = app.listen(Number(process.env.PORT || 5000), function() {
+var server = app.listen(Number(process.env.PORT || config.get('ics.port')), function() {
   console.log('NODE_ENV=%s http://%s:%d', app.settings.env, server.address().address, server.address().port);
 });

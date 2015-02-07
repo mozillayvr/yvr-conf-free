60cf984ac865762285500c6faacf0664002cc58d
Bryan Clark <clarkbw@gmail.com>
2015-02-06 16:03:27 -0800

    use path with the static directory serving

--- server.js
+++ server.js
@@ -8,10 +8,11 @@
 "use strict";
 
 var config = require('config');
+var path = require('path');
 
 var app = require('gcal-conf-free-api');
 
-app.use(require('express').static(__dirname + '/public'));
+app.use('/', require('express').static(path.join(__dirname, 'public')));
 
 var server = app.listen(Number(process.env.PORT || config.get('ics.port')), function() {
   console.log('NODE_ENV=%s http://%s:%d', app.settings.env, server.address().address, server.address().port);
